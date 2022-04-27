3299866ba8350789baca099576dda41f10b85976
Juan Betancourt <juancbetancourt11@gmail.com>
2022-04-27 13:38:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Different Functions
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+#Defining Functions
+def my_function():
+  print("Hello")
+  print("Bye")
+
+#Calling the Function
+my_function()
